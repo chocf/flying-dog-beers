2245b24173a878d6f83bb0b75768dacab4e1a6a9
chocf <58251147+chocf@users.noreply.github.com>
2019-11-28 15:15:54 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -42,7 +42,7 @@ beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, url_base_pathname='/app2')
 server = app.server
 app.title=tabtitle
 
